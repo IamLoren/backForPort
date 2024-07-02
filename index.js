8d9314afb7d9352d50ca2af5a4c2107fdab1f8b6
Iryna Kostromytska <neopoznannyi.enot@gmail.com>
2024-07-02 15:22:04 +0300

    fix math

--- index.js
+++ index.js
@@ -1,53 +1,67 @@
 import { log } from "console";
 import fs from "fs/promises";
 
-const read = async () => {
+export const read = async () => {
+  let accumulator = 0;
+  let sum = 0;
+  let mediana;
+  let message =
+    "З усіх значень у Вашому файлі отримано числа та розраховано запитувані параметри";
+
+    let resp = {};
   try {
-    const text = await fs.readFile("./files/num.txt", "utf-8");
+    const text = await fs.readFile("./files/numbers.txt", "utf-8");
+    if (!text.trim()) {
+      throw new Error("У завантаженому файлі чисел не знайдено");
+    }
 
     const numbers = text.split("\n").map((num) => parseInt(num));
+    const filteredNumbers = numbers.filter((num) => {
+      if (isNaN(num)) {
+        message =
+          "У Вашому списку виявлено нечислові символи. Вони будуть виключені з розрахунку запитуваних параметрів";
+      }
+      return !isNaN(num);
+    });
 
-    let accumulator = 0;
-    let sum = 0;
-    let mediana;
-    let max = numbers[0];
-    let min = numbers[0];
+    let max = filteredNumbers[0];
+    let min = filteredNumbers[0];
+    let length = filteredNumbers.length;
 
-    for (let i = 0; i < numbers.length; i++) {
-      accumulator += numbers[i];
-      console.log(accumulator)
-      if (
-        numbers.length % 2 === 0 &&
-        (i === numbers.length / 2 - 1 || i === numbers.length / 2)
-      ) {
-        sum += numbers[i];
+    for (let i = 0; i < length; i++) {
+      accumulator += filteredNumbers[i];
+
+      if (length % 2 === 0 && (i === length / 2 - 1 || i === length / 2)) {
+        sum += filteredNumbers[i];
         console.log(sum);
-      } else {
-        let index = Math.floor(numbers.length / 2);
-        mediana = numbers[index];
+        mediana = sum / 2;
+      } else if (length % 2 !== 0) {
+        let index = Math.floor((filteredNumbers.length - 1) / 2);
+        mediana = filteredNumbers[index];
       }
-      mediana = sum / 2;
-      if (numbers[i] > max) {
-        max = numbers[i];
+
+      if (filteredNumbers[i] > max) {
+        max = filteredNumbers[i];
       }
-      if (numbers[i] < min) {
-        min = numbers[i];
+      if (filteredNumbers[i] < min) {
+        min = filteredNumbers[i];
       }
     }
-    let average = accumulator / numbers.length;
-    console.log(
-      "Медіана:",
+
+    let average = accumulator / filteredNumbers.length;
+
+     resp = {
+      message,
       mediana,
-      "average:",
       average,
-      "max:",
       max,
-      "min:",
       min
-    );
+    }; 
+   console.log(resp)
   } catch (error) {
     console.error("Помилка:", error.message);
   }
+  return resp;
 };
 
-read();
+read ()
